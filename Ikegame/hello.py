7b0643500c6db5a84e4bae0f221bde01a903a2d8
Sohei-Ikegame <ikegame.sohei.kz@mynavi.jp>
2023-06-01 01:59:46 +0000

    hello flask

--- Ikegame/hello.py
+++ Ikegame/hello.py
@@ -1,1 +1,10 @@
-print("Hello Miraina Tower")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello Miraina Tower"
+
+if __name__ == "__main__":
+    app.run()
+    

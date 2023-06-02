8b7269df76eb66791397bfd8fa630c29303d8d7f
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-06-02 01:58:56 +0000

    きゅうよけいさん

---
+++ kawamura/calcsalary_app/salary/views/views.py
@@ -0,0 +1,30 @@
+from salary import app
+from flask import request, url_for, render_template,flash, session, redirect
+import sys
+from decimal import Decimal, ROUND_HALF_UP
+
+@app.route('/')
+def show_entries():
+    return render_template('input.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    sikyu = 0
+    tax = 0
+    if request.method=='POST':
+        if str(request.form['username']) == "":
+            flash('値を入力しろ')
+            return render_template('input.html')
+        if int(request.form['username']) > 1000000 :
+            tax = Decimal(100000 + ( int(request.form['username']) - 1000000)* 0.2).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+            sikyu = int(request.form['username']) - tax
+            print("支給額:" + str(sikyu) + "、", end="")
+            print("税額:" + str(tax), end="")
+
+        else :
+            tax = Decimal(int(request.form['username'])*0.1).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+            sikyu = int(request.form['username']) - tax
+            print("支給額:" + str(sikyu) + "、", end="")
+            print("税額:" + str(tax), end="")
+            
+    return render_template('output.html',kyuyo = request.form['username'], result = sikyu, zei = tax)

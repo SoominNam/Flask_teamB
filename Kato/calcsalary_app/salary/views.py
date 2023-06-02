263867fbc2090789fa5972ee802ec976b2a4cc8d
YoshitakaKato-web <kato.yoshitaka.sp@mynavi.jp>
2023-06-02 02:41:02 +0000

    add validations

--- Kato/calcsalary_app/salary/views.py
+++ Kato/calcsalary_app/salary/views.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, url_for, render_template, session
+from flask import request, redirect, url_for, render_template, session, flash
 from salary import app
 from decimal import Decimal, ROUND_HALF_UP
 
@@ -9,14 +9,22 @@ def main():
 
 @app.route('/output', methods=['GET', 'POST'])
 def input():
-    salary = int(request.form["salary"])
+    len_salary = len(str(request.form["salary"]))
 
     if request.method == 'POST':
-        if  salary > 1000000:
-           tax = (salary - 1000000) * 0.2 + 1000000 * 0.1
-           expenses = salary - tax
+        if not request.form["salary"]:
+            flash("給与が未入力です。入力してください。")
+        elif len_salary >= 10:
+            flash("給与には最大9,999,999,999まで入力可能です。")
+        elif int(request.form["salary"]) < 0:
+            flash("給与にはマイナスの値は入力できません。")
         else:
-            tax = salary * 0.1
-            expenses = salary - tax
-    return render_template('output.html', salary = salary, tax = tax, expenses = expenses)
-   
+            salary = int(request.form["salary"])
+            if  salary > 1000000:
+                tax = (salary - 1000000) * 0.2 + 1000000 * 0.1
+                expenses = salary - tax
+            else:
+                tax = salary * 0.1
+                expenses = salary - tax
+            return render_template('output.html', salary = salary, tax = tax, expenses = expenses)
+    return redirect("/")

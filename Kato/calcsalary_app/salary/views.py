216ff3a2e7963a3f2e18c56f74a1acd9ab5f96dd
YoshitakaKato-web <kato.yoshitaka.sp@mynavi.jp>
2023-06-02 04:44:50 +0000

    fix views.py

--- Kato/calcsalary_app/salary/views.py
+++ Kato/calcsalary_app/salary/views.py
@@ -21,10 +21,10 @@ def input():
         else:
             salary = int(request.form["salary"])
             if  salary > 1000000:
-                tax = (salary - 1000000) * 0.2 + 1000000 * 0.1
+                tax = Decimal(str((salary - 1000000) * 0.2 + 1000000 * 0.1)).quantize(Decimal("0"), rounding=ROUND_HALF_UP)
                 expenses = salary - tax
             else:
-                tax = salary * 0.1
+                tax = Decimal(str(salary * 0.1)).quantize(Decimal("0"), rounding=ROUND_HALF_UP)
                 expenses = salary - tax
             return render_template('output.html', salary = salary, tax = tax, expenses = expenses)
     return redirect("/")

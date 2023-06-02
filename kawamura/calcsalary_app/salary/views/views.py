9cdcef4fe881c86fba7fe8574ffdd4b9f710c785
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-06-02 04:14:30 +0000

    バリデーションチェック

--- kawamura/calcsalary_app/salary/views/views.py
+++ kawamura/calcsalary_app/salary/views/views.py
@@ -12,19 +12,27 @@ def login():
     sikyu = 0
     tax = 0
     if request.method=='POST':
-        if str(request.form['username']) == "":
-            flash('値を入力しろ')
+        if request.form['username'] == "":
+            flash('入力しろ')
             return render_template('input.html')
-        if int(request.form['username']) > 1000000 :
-            tax = Decimal(100000 + ( int(request.form['username']) - 1000000)* 0.2).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
-            sikyu = int(request.form['username']) - tax
-            print("支給額:" + str(sikyu) + "、", end="")
-            print("税額:" + str(tax), end="")
+        if len(request.form['username']) >= 11:
+            flash('10字以内にしろ')
+            return render_template('input.html')
+        if int(request.form['username']) < 0:
+            flash('マイナスはおかしい')
+            return render_template('input.html')
 
-        else :
-            tax = Decimal(int(request.form['username'])*0.1).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
-            sikyu = int(request.form['username']) - tax
-            print("支給額:" + str(sikyu) + "、", end="")
-            print("税額:" + str(tax), end="")
+        else:
+            if int(request.form['username']) > 1000000 :
+                tax = Decimal(100000 + ( int(request.form['username']) - 1000000)* 0.2).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+                sikyu = int(request.form['username']) - tax
+                print("支給額:" + str(sikyu) + "、", end="")
+                print("税額:" + str(tax), end="")
+
+            else :
+                tax = Decimal(int(request.form['username'])*0.1).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+                sikyu = int(request.form['username']) - tax
+                print("支給額:" + str(sikyu) + "、", end="")
+                print("税額:" + str(tax), end="")
             
     return render_template('output.html',kyuyo = request.form['username'], result = sikyu, zei = tax)
